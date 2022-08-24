953230ad0f4192a406294f2a09bcec2677fd5363
tonyWh1te <zmyshenko.valery@bk.ru>
2022-08-24 22:32:47 +0300

    adding babel and updating webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,46 @@
 
 let path = require('path');
 
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
 module.exports = {
-  mode: 'development',
-  entry: './js/index.js',
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/js',
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   watch: true,
 
-  devtool: 'source-map',
-
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  debug: true,
+                  corejs: '3.6.5',
+                  useBuiltIns: 'usage',
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
